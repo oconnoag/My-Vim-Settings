e8328295158c9ea8881c5486133d3a893921f2fc
Alijah O'Connor <alijah.oconnor@colorado.edu>
2019-08-09 13:13:27 -0600

    adding nerdtree and syntastic

--- setup.sh
+++ setup.sh
@@ -1,7 +1,16 @@
 # Set up main .vimrc (this file will pull in features from this .vim 
 #  - I have it set up this way so that I can version control my .vimrc file easily
-mv ~/.vim/save_this_as_dotvimrc_in_home_dir ~/.vimrc
+# (Only perform if that file exists)
+FILE=~/.vim/save_this_as_dotvimrc_in_home_dir
+if test -f "$FILE"; then
+    mv $FILE ~/.vimrc
+fi
 
 ####################################################################################################
 # Set up plugins
 ####################################################################################################
+# Nerdtree
+cd ~/.vim/bundle/ && git clone https://github.com/scrooloose/nerdtree.git
+
+# Syntastic
+cd ~/.vim/bundle/ && git clone https://github.com/vim-syntastic/syntastic.git
