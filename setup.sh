caaaed58bc49b45b9c2283a312abd6600303a9c5
Alijah O'Connor <aoconnor@archerdx.com>
2019-08-20 16:37:33 -0600

    set up buffer configs

--- setup.sh
+++ setup.sh
@@ -29,3 +29,13 @@ cd ~/.vim/bundle && git clone https://github.com/ctrlpvim/ctrlp.vim
 
 # vim-polyglot
 cd ~/.vim/bundle && git clone https://github.com/sheerun/vim-polyglot.git
+
+# gruvbox colorscheme
+cd ~/.vim/bundle && git clone https://github.com/morhetz/gruvbox.git
+
+# buffergator
+cd ~/.vim/bundle && git clone https://github.com/jeetsukumaran/vim-buffergator.git
+
+# vim-airline
+cd ~/.vim/bundle && git clone https://github.com/vim-airline/vim-airline.git
+cd ~/.vim/bundle && git clone https://github.com/vim-airline/vim-airline-themes 
