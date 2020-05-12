3c00feaa76f1e0d730eebbcda05c0a4a94dd90a7
Alijah O'Connor <aoconnor@archerdx.com>
2020-05-12 14:05:49 -0600

    Better dependency control for setup.py

--- setup.sh
+++ setup.sh
@@ -22,7 +22,6 @@ git clone https://github.com/tomtom/tcomment_vim.git
 git clone https://github.com/ervandew/supertab.git
 
 # ctrlp
-brew install ripgrep  # Faster searching than ag
 git clone https://github.com/ctrlpvim/ctrlp.vim
 
 # vim-polyglot
@@ -39,7 +38,6 @@ git clone https://github.com/vim-airline/vim-airline.git
 git clone https://github.com/vim-airline/vim-airline-themes 
 
 # gutentags (requires universal-ctags)
-brew install --HEAD universal-ctags/universal-ctags/universal-ctags
 git clone https://github.com/ludovicchabant/vim-gutentags.git
 
 # w0rp/ale
@@ -47,3 +45,17 @@ git clone https://github.com/dense-analysis/ale.git
 
 # simpylfold - Python code folding
 git clone https://github.com/tmhedberg/SimpylFold.git
+
+# Other dependencies (will only install if brew is installed)
+if [ ! -z brew ]; then
+    # Faster searching than ag
+    [[ ! -z ripgrep ]] || brew install ripgrep
+
+    # ctags for glutentags
+    brew install --HEAD universal-ctags/universal-ctags/universal-ctags
+
+    # Linters for ALE
+    [[ ! -z flake8 ]] || brew install flake8
+    [[ ! -z pylint ]] || brew install pylint
+fi
+
